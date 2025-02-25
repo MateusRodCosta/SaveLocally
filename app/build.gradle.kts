b6c94b1d979b9d1b5ed3afc375edcd7cb801d69d
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-02-25 16:30:11 -0300

    Prepare 1.4.3 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 28
-        versionName = "1.4.2"
+        versionCode = 29
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
