01e3ad4cb9dfb21dd2f89d718ab0478878ed395a
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-04-26 16:30:21 -0300

    Upgrade Gradle, AGP and Dependency analysis plugin
    Gradle: 8.13 -> 8.14
    AGP: 8.9.1 -> 8.9.2
    com.autonomousapps.build-health: 2.13.3 -> 2.17.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,11 +16,11 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.autonomousapps.build-health") version "2.13.3"
+    id("com.autonomousapps.build-health") version "2.17.0"
 
     id("org.jetbrains.kotlin.jvm") version "2.1.20" apply false
 
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.9.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
 
