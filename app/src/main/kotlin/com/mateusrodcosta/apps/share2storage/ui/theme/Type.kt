2bf8d7addf56444a7f6fb22c160b7371cf5010b2
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2026-05-15 17:17:57 -0300

    Migrate to FiraGO font

--- app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/ui/theme/Type.kt
+++ app/src/main/kotlin/com/mateusrodcosta/apps/share2storage/ui/theme/Type.kt
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2024 Mateus Rodrigues Costa
+ *     Copyright (C) 2024 - 2026 Mateus Rodrigues Costa
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as
@@ -18,5 +18,37 @@
 package com.mateusrodcosta.apps.share2storage.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.mateusrodcosta.apps.share2storage.R
 
-val AppTypography = Typography()
+val firaGoFamily = FontFamily(
+    Font(R.font.firago_regular, FontWeight.Normal),
+    Font(R.font.firago_medium, FontWeight.Medium),
+    Font(R.font.firago_bold, FontWeight.Bold)
+)
+
+val bodyFontFamily = firaGoFamily
+val displayFontFamily = firaGoFamily
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val AppTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+)
