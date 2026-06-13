e3c3f28ec7ef0338b5cff64ff71bc0c5a5623043
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2026-06-13 12:53:53 -0300

    Prepare 2.1.1 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 28
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 33
-        versionName = "2.1.0"
+        versionCode = 34
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
