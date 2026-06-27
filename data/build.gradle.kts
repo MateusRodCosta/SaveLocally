a38919de6f23d0252f0eb2c336098c8cbd8b900e
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2026-06-26 21:09:15 -0300

    Upgrade :data compileSdk to 37.0
    compileSdk: 36.1 -> 37.0

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 android {
     namespace = "com.mateusrodcosta.apps.share2storage.data"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
